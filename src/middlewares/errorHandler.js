73ff8acf33e49ea27b06077e67d985e1ac0b8fc6
Giovanni <giovannibarolin@gmail.com>
2023-05-22 10:34:03 -0300

    feat: adding errorHandler function

---
+++ src/middlewares/errorHandler.js
@@ -0,0 +1,15 @@
+const ERRORS={
+    "Bad Request":400,
+    "Unauthorized":401,
+    "Forbidden":403,
+    "Not found":404,
+}
+
+export const errorHandler = (err,req,res,next)=>{
+    
+    if(err?.name.includes('ZodError')){
+        return res.status(400).send({status:'error',message:err.issues});
+    }
+    return res.status(ERRORS[err?.cause] ?? 500).send({status:'error',message:err.message});
+   
+}
