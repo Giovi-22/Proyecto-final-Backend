9da6c8b60507127eb4ac0625da69331c6cca3eef
Giovanni <giovannibarolin@gmail.com>
2023-06-29 17:37:47 -0300

    adding auth to addOne method on cart route

--- src/presentation/routes/cartRouter.js
+++ src/presentation/routes/cartRouter.js
@@ -10,7 +10,7 @@ const cartRouter = Router();
 cartRouter.get('/',auth,authorization('getCarts'),CartController.getAll);// <devuelve todos los carritos>
 cartRouter.get('/:cid',auth,CartController.get);                     // <devuelve el carrito seleccionado pasandole por params el cid>
 cartRouter.post('/',CartController.create);                         // <crea el carrito con un array products vacío>
-cartRouter.post('/:cid/product/:pid',CartController.addOne);        // <agrega un producto al carrito, con quantity = 1>
+cartRouter.post('/:cid/product/:pid',auth,CartController.addOne);        // <agrega un producto al carrito, con quantity = 1>
 cartRouter.put('/:cid/',CartController.updateCart);                 // <actualiza el carrito pasando por body los productos con el siguiente formato: [{"pid":<valor>,"quantity":<valor>},...]>
 cartRouter.put('/:cid/product/:pid',CartController.updateOne);      // <actualiza el carrito pasando por body la cantidad: {"quantity":<valor>} >
 cartRouter.delete('/:cid',CartController.deleteAll);                // <borra todos los productos del carrito seleccionado (cid)>
