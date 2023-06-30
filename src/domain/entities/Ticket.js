75b8fd54949386b600b7121559e58b73b2d17031
Giovanni <giovannibarolin@gmail.com>
2023-06-30 20:14:19 -0300

    feat: creating the Ticket entity and the TicketManager

---
+++ src/domain/entities/Ticket.js
@@ -0,0 +1,13 @@
+
+class Ticket{
+
+    constructor(props){
+    this.#id = props.id;
+    this.#code = props.code;
+    this.#purchase_datetime = props.purchase_datetime;
+    this.#amount = props.amount;
+    this.#purchaser = props.purchaser;
+    }
+}
+
+export default Ticket;
---
+++ src/domain/managers/TicketManager.js
@@ -0,0 +1,15 @@
+import Ticket from "../entities/Ticket";
+
+class TicketManager{
+
+
+    constructor(){
+        this.Ticket = new Ticket();
+    }
+
+    async generate(){
+
+    }
+}
+
+export default TicketManager;
