6fb108816450eb695f2c809af8bbb023326473e6
Giovanni <giovannibarolin@gmail.com>
2023-08-18 22:20:42 -0300

    creating the generateProduct() function to generate a fake product using faker

--- src/helpers/fakers.js
+++ src/helpers/fakers.js
@@ -15,4 +15,17 @@ export const generateUser = ()=>{
     }
 }
 
+export const generateProduct = ()=>{
+    return {
+        title: `Cortante ${faker.commerce.product()}`,
+        description: `${faker.commerce.productDescription()}, Tamaño ${faker.number.int({min:3,max:10})}`,
+        price: faker.number.int({min:250,max:1500}),
+        thumbnail: [],
+        code: `CB${faker.number.int({min:1,max:500})}`,
+        stock: faker.number.int({min:1,max:100}),
+        status: true,
+        category:"cortantes",
+      }
+}
+
 //role:"647fd20fb16b39892de4c6aa",
