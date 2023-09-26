09849d910ca7a843e9aaab55982c8215ad50d5f3
Giovanni <giovannibarolin@gmail.com>
2023-09-25 23:55:48 -0300

    changing the bcrypt file to typescript

--- src/shared/bcrypt.ts
+++ src/shared/bcrypt.ts
@@ -1,12 +1,12 @@
 import bcrypt from 'bcrypt';
 
-export const hashPassword = async (password)=>
+export const hashPassword = async (password:string)=>
 {
     const hashedPassword = await bcrypt.hash(password,10);
     return hashedPassword; 
 }
 
-export const verifyPassword = async (userDBPassword,password)=>
+export const verifyPassword = async (userDBPassword:string,password:string)=>
 {
     const isHashedPassword = await bcrypt.compare(password,userDBPassword);
     return isHashedPassword;
